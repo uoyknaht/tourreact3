be413b8c3ef531895eb02eee724401deaec64db1
Paulius R <abelardinijo@gmail.com>
2016-01-29 00:34:33 +0200

    struggling with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,68 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: {
     main: [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
       './client/src/app.js'
     ]
   },
   output: {
-    filename: './client/dist/app.js'
+    filename: 'app.js',
+    path: path.join(__dirname, 'client', 'dist'),
+    publicPath: '/client/dist/'
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        include: path.join(__dirname, 'client', 'src'),
+        loader: 'react-hot!babel'
       }
     ]
   }
 }
+
+
+// examples
+//
+// module.exports = {
+//   context: __dirname + '/app',
+//   entry: {
+//     javascript: "./app.js",
+//     html: "./index.html"
+//   },
+//   output: {
+//     filename: 'app.js',
+//     path: __dirname + '/dist'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loaders: ['babel-loader']
+//       },
+//       {
+//         test: /\.jsx$/,
+//         loaders: ['babel-loader']
+//       },
+//       {
+//         test: /\.html$/,
+//         loader: "file?name=[name].[ext]"
+//       }
+//     ]
+//   }
+// };
