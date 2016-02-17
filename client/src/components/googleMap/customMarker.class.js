dadeecf0fbd9aabfd33dcca8b160aa919ecfb228
Paulius R <abelardinijo@gmail.com>
2016-02-17 20:27:14 +0200

    Draggin works

--- client/src/components/googleMap/customMarker.class.js
+++ client/src/components/googleMap/customMarker.class.js
@@ -1,7 +1,7 @@
 // http://humaan.com/custom-html-markers-google-maps/
 
 function CustomMarker(latLng, map, args) {
-    this.latlng = new google.maps.LatLng(latLng.lat, latLng.lng);   
+    this.latlng = new google.maps.LatLng(latLng.lat, latLng.lng);
     this.args = args || {};
     this.setMap(map);
     this.map = map;
@@ -11,27 +11,27 @@ CustomMarker.prototype = new google.maps.OverlayView();
 
 CustomMarker.prototype.onAdd = function() {
     var self = this;
-    
+
     var div = this.div;
-    
+
     if (!div) {
-    
+
         div = this.div = document.createElement('div');
-        
+
         div.className = this.args.className || 'marker';
-        
+
         div.style.position = 'absolute';
         div.style.width = '20px';
         div.style.height = '20px';
         // http://jsfiddle.net/doktormolle/QRuW8/
         // https://developers.google.com/maps/documentation/javascript/customoverlays
         div.draggable = true;
-        
+
         if (typeof(self.args.marker_id) !== 'undefined') {
             div.dataset.marker_id = self.args.marker_id;
         }
-        
-        google.maps.event.addDomListener(div, "click", function(event) {            
+
+        google.maps.event.addDomListener(div, "click", function(event) {
             google.maps.event.trigger(self, "click");
         });
 
@@ -44,48 +44,23 @@ CustomMarker.prototype.onAdd = function() {
                 that.map.set('draggable', false);
                 that.set('origin', e);
 
-                that.moveHandler = google.maps.event.addDomListener(that.get('map').getDiv(),
+                that.moveHandler = google.maps.event.addDomListener(
+					that.get('map').getDiv(),
                     'mousemove',
                     function(e) {
                         var origin = that.get('origin');
                         var left = origin.clientX - e.clientX;
-                        var top = origin.clientY - e.clientY;                    
-                        // var point = that.getProjection().fromLatLngToDivPixel(latLng);
-
+                        var top = origin.clientY - e.clientY;
                         var lat = that.getPosition().lat();
                         var lng = that.getPosition().lng();
-                        var latLng =  new google.maps.LatLng(lat, lng); 
-
-                        // var pos = that.getProjection().fromLatLngToDivPixel(that.get('position'));
+                        var latLng =  new google.maps.LatLng(lat, lng);
                         var pos = that.getProjection().fromLatLngToDivPixel(latLng);
-
+						var point = new google.maps.Point(pos.x - left, pos.y - top);
+						var latLng2 = that.getProjection().fromDivPixelToLatLng(point);
                         that.set('origin', e);
-// debugger;
-                        that.latlng = latLng;
-                        // that.set('position', latLng);
+                        that.latlng = latLng2;
                         that.draw();
-
-
-                        // console.log(pos);
-
-// // debugger;
-//                         // var pos = that.getProjection();
-//                         console.log(that.get('position'));
-//                         console.log(that);
-
-
-
-                        //     
-                        //     .fromLatLngToDivPixel(that.get('position')),
-                        //     latLng = that.getProjection()
-                        //     .fromDivPixelToLatLng(new google.maps.Point(pos.x - left, pos.y - top));
-
-                        // that.set('origin', e);
-                        // that.set('position', latLng);
-                        // that.draw();
                     });
-
-
             }
         );
 
@@ -95,25 +70,18 @@ CustomMarker.prototype.onAdd = function() {
             google.maps.event.removeListener(that.moveHandler);
         });
 
-
-
-
-
-
-
-        
         var panes = this.getPanes();
         panes.overlayImage.appendChild(div);
     }
 };
 
 CustomMarker.prototype.draw = function() {
-    
     var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
-    
+	console.log(point);
+
     if (point) {
-        div.style.left = point.x + 'px';
-        div.style.top = point.y + 'px';
+        this.div.style.left = point.x + 'px';
+        this.div.style.top = point.y + 'px';
     }
 };
 
@@ -121,11 +89,11 @@ CustomMarker.prototype.remove = function() {
     if (this.div) {
         this.div.parentNode.removeChild(this.div);
         this.div = null;
-    }   
+    }
 };
 
 CustomMarker.prototype.getPosition = function() {
-    return this.latlng; 
+    return this.latlng;
 };
 
 export default CustomMarker;
