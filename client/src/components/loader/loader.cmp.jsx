9efe6361433e31356e5e51cc8ace3f264d32b58f
Paulius R <abelardinijo@gmail.com>
2016-04-02 11:59:30 +0300

    disabled loader for now

--- client/src/components/loader/loader.cmp.jsx
+++ client/src/components/loader/loader.cmp.jsx
@@ -11,14 +11,15 @@ class Loader extends React.Component {
           //   <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{width: 40 + '%'}}>
           //   </div>
           // </div>
+            // <div className="tr-loader">
+            //     <div className="tr-loader-cover"></div>
+            //     <div className="tr-loader-spinner-wrap">
+            //         <div className="tr-loader-spinner"></div>
+            //     </div>
+            // </div>          
         return (
-            <div className="tr-loader">
-                <div className="tr-loader-cover"></div>
-                <div className="tr-loader-spinner-wrap">
-                    <div className="tr-loader-spinner"></div>
-                </div>
-            </div>
-        );
+            <div></div>
+        )
     }
 
 }
--- server/queries/customQueries.js
+++ server/queries/customQueries.js
@@ -4,7 +4,10 @@ var updateCategoryProperty = require('./updateCategory');
 function doCustomQueries(req, res, next) {
     // console.log('------------------------')
     // addCategory(req, res, next);
-    // updateCategoryProperty(req, res, next, 'className', 'castle', 'color', '#bf0000');
+    
+    // updateCategoryProperty(req, res, next, 'slug', 'castles', 'title', 'Castles');
+    // updateCategoryProperty(req, res, next, 'title', 'Beaches', 'slug', 'beaches');
+
     // updateCategoryProperty('className', 'castle', 'color', '#7f0000');
     // updateCategoryProperty('className', 'beach', 'color', '#086ad3');       
 }
